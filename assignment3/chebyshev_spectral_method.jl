23e770b8572ea0ba863ada4d84798e7365bfdf5f
王泽远 <1692167054@qq.com>
2024-04-11 15:58:34 -0600

    update assignment 3

--- assignment3/chebyshev_spectral_method.jl
+++ assignment3/chebyshev_spectral_method.jl
@@ -14,46 +14,68 @@ dTdx(n,x) = (n <= 1) ? n : 2x *dTdx(n-1,x) + 2*T(n-1,x) - dTdx(n-2,x)
 #d2Tdx2(n,x) = (n <= 1) ? 0 : 2x * d2Tdx2(n-1,x) + 4 * dTdx(n-1,x) - d2Tdx2(n-2,x)
 d2Tdx2(n,x) = -n*n * (cos(n * acos(x))/(1 - x*x)) + ((n * x * sin(n * acos(x)))/sqrt(((1-x*x)^3)))
 
-u(x) = cos(pi/2 * x)
-f(x) = pi*pi/4*cos(pi/2 * x)
+#u(x) = cos(pi/2 * x)
+#f(x) = (pi^2)/4*cos(pi/2 * x)
 
-n = 5  # Number of extrema (degree of the Chebyshev polynomial)
-extrema = nodes(n,:chebyshev_extrema).points
+w1 = pi
+w2 = 2pi
+w3 = 4pi
+w4 = 8pi
+w5 = 16pi
+w6 = 32pi
+w7 = 64pi
+w8 = 128pi
 
-Tmat = zeros(n,n)
+u(x) = sin(w1 *x) + sin(w2 * x) + sin(w3 * x) + sin(w4 * x) + sin(w5 * x) + sin(w6 * x) + sin(w7* x) + sin(w8 * x)
+f(x) = (w1)^2 * sin(w1*x) + (w2)^2 * sin(w2*x) + (w3)^2 * sin(w3*x) + (w4)^2 * sin(w4*x) + (w5)^2 * sin(w5*x) + (w6)^2 * sin(w6*x) + (w7)^2 * sin(w7*x) + (w8)^2 * sin(w8*x)
 
-for j in 1 : n 
-    Tmat[1, j] = T(j-1,extrema[1])
-    Tmat[n, j] = T(j-1,extrema[n])
-end
+frame = 0
 
-for i in 2:n-1
-    for j in 1:n
-        Tmat[i, j] = d2Tdx2(j-1,extrema[i])
+for n in 20 : 2000 # Number of extrema (degree of the Chebyshev polynomial)
+    extrema = nodes(n,:chebyshev_extrema).points
+    
+    Tmat = zeros(n,n)
+    
+    for j in 1 : n 
+        Tmat[1, j] = T(j-1,extrema[1])
+        Tmat[n, j] = T(j-1,extrema[n])
     end
+    
+    for i in 2:n-1
+        for j in 1:n
+            Tmat[i, j] = d2Tdx2(j-1,extrema[i])
+        end
+    end
+    
+    b = zeros(n)
+    
+    b[1] = b[n] = 0
+    
+    for j in 2 : n-1
+        b[j] = -f(extrema[j])
+    end
+    
+    println("Solve for coefficients...")
+    c = Tmat \ b
+    println("Solve done.")
+    
+    u_1(x) = sum(c[i] * T(i-1,x) for i in 1:length(c))
+    
+    test_x = range(-1, stop=1, length=1000)
+    
+    if frame == 0
+        plt = plot(test_x, u.(test_x),ylims = (-4,4))
+        plot!(plt,test_x, u_1.(test_x))
+        savefig(plt,"res/res_$n.png")
+    end
+    #display(plt)
+    # Prevent process close too soon
+    # Presse enter or use ctrl + C to exit the process
+    #readline()
+    println("n = $n")
+    u_ref = u.(test_x)
+    error = u_ref  - u_1.(test_x)
+    println(norm(error,2)/norm(u_ref,2))
+    println(norm(error,Inf)/norm(u_ref,Inf))
+    global frame = (frame + 1) % 100
 end
-
-b = zeros(n)
-
-b[1] = b[n] = 0
-
-for j in 2 : n-1
-    b[j] = -f(extrema[j])
-end
-
-println("Solve for coefficients...")
-c = Tmat \ b
-println("Solve done.")
-
-u_1(x) = sum(c[i] * T(i-1,x) for i in 1:length(c))
-
-test_x = range(-1, stop=1, length=1000)
-
-u_ref = u.(test_x)
-
-plt = plot(test_x, u.(test_x))
-plot!(plt,test_x, u_1.(test_x))
-display(plt)
-# Prevent process close too soon
-# Presse enter or use ctrl + C to exit the process
-readline()
--- assignment3/crank_nicolson.jl
+++ assignment3/crank_nicolson.jl
@@ -1,25 +1,27 @@
 using Pkg
 Pkg.add("Plots")
 Pkg.add("IterativeSolvers")
+Pkg.add("Preconditioners")
 using Plots
 using LinearAlgebra
 using IterativeSolvers
 using SparseArrays
+using Preconditioners
 
 #nu = 1/(2 * pi)
 
-decay = pi/2
-freq = 1 * pi
-nu = 0.0001
-u(x,y,t) = sin(freq*x)cos(freq*y)*exp(-decay*t) + 1.0
-f(x,y,t) = (2 * freq^2 * nu - decay)sin(freq*x)cos(freq*y)*exp(-decay*t)
+decay = pi
+freq = pi
+nu = 1.0
+u(x,y,t) = sin(freq*x)*cos(freq*y)*exp(-decay*t) + 1.0
+f(x,y,t) = (2 * freq^2 * nu - decay)*sin(freq*x)*cos(freq*y)*exp(-decay*t)
 
-grid_size = 401
-h = 1.0/Float64(grid_size-1)
+grid_size = 101
+h = 1.0/(grid_size-1)
 dt = 0.001#2 * h^2/nu #0.001
 
-alpha1 = 1 + (2.0 * nu * dt)/(h^2)
-alpha2 = 1 - (2.0 * nu * dt)/(h^2)
+alpha1 = 1.0 + (2.0 * nu * dt)/(h^2)
+alpha2 = 1.0 - (2.0 * nu * dt)/(h^2)
 beta = (nu * dt)/(2.0*h^2) 
 
 # Construct the L matrix(without boundray)
@@ -44,35 +46,54 @@ for i in 1 : grid_size_in
         idx_down = get_idx(i,j-1,grid_size_in)
         idx_up = get_idx(i,j+1,grid_size_in)
 
-        if idx_left >= 1 && idx_left <= N_in
+        if j == 1 
+            idx_down = 0
+        end
+
+        if j == grid_size_in
+            idx_up = 0
+        end
+
+        if i == 1
+            idx_left = 0
+        end
+
+        if i == grid_size_in
+            idx_right = 0
+        end
+
+        if idx_left != 0
             push!(rows,idx0)
             push!(cols,idx_left)
             push!(vals,-beta)
         end
 
-        if idx_right >= 1 && idx_right <= N_in
+        if idx_right != 0
             push!(rows,idx0)
             push!(cols,idx_right)
             push!(vals,-beta)
         end
 
-        if idx_down >= 1 && idx_down <= N_in
+        if idx_down != 0
             push!(rows,idx0)
             push!(cols,idx_down)
             push!(vals,-beta)
         end
 
-        if idx_up >= 1 && idx_up <= N_in
+        if idx_up != 0
             push!(rows,idx0)
             push!(cols,idx_up)
             push!(vals,-beta)
         end
-
     end
 end
 
 L = sparse(rows, cols, vals, N_in, N_in)
 @assert issymmetric(L)
+@assert isposdef(L)
+
+# Incomplete Cholesky preconditioner with cut-off level 2
+PreConL = CholeskyPreconditioner(L, 2)
 
 u_n = zeros(N_in)
 rhs = zeros(N_in)
@@ -87,7 +108,7 @@ end
 
 # Start simulation
 frame = 0
-for n in 0 : Int(1.0/dt)
+for n in 0 : Int(.5/dt)
     tn  = dt * n
     tn1 = tn + dt
     println("t : ",tn)
@@ -109,62 +130,62 @@ for n in 0 : Int(1.0/dt)
                 @assert x - h == 0.0
                 u_n_left = u(0,y,tn) # Use Dirichelet boundary condition
             else
-                u_n_left = u_n[get_idx(i - 1,j,grid_size_in)]#u(x - h, y, tn)#u_n[get_left_idx(i,j,grid_size_in)]
+                u_n_left = u_n[get_idx(i - 1,j,grid_size_in)]
             end
 
             if i == grid_size_in
                 @assert x + h == 1.0
                 u_n_right = u(1.0,y,tn) # Use Dirichelet boundary condition
             else
-                u_n_right = u_n[get_idx(i + 1,j,grid_size_in)]#u(x + h, y, tn)#u_n[get_right_idx(i,j,grid_size_in)]
+                u_n_right = u_n[get_idx(i + 1,j,grid_size_in)]
             end
 
             if j == 1 
                 @assert y - h == 0.0
                 u_n_down = u(x, 0 ,tn) # Use Dirichelet boundary condition
             else
-                u_n_down = u_n[get_idx(i,j - 1,grid_size_in)]#u(x, y - h, tn)#u_n[get_down_idx(i,j,grid_size_in)]
+                u_n_down = u_n[get_idx(i,j - 1,grid_size_in)]
             end
 
             if j == grid_size_in 
                 @assert y + h == 1.0
-                u_n_up = u(x,0.0,tn) # Use Dirichelet boundary condition
+                u_n_up = u(x,1.0,tn) # Use Dirichelet boundary condition
             else
-                u_n_up = u_n[get_idx(i,j + 1,grid_size_in)]#u(x , y + h, tn)#u_n[get_up_idx(i,j,grid_size_in)]
+                u_n_up = u_n[get_idx(i,j + 1,grid_size_in)]
             end
-            
-            global rhs[idx0] = alpha2 * u_n_center + beta * (u_n_left + u_n_right + u_n_down + u_n_up) + dt * 0.5 * (f(x, y, tn) + f(x, y, tn1))
+        
+            rhs[idx0] = alpha2 * u_n_center + beta * (u_n_left + u_n_right + u_n_down + u_n_up) + dt * 0.5 * (f(x, y, tn) + f(x, y, tn1))
 
             if i == 1 
                 @assert x - h == 0.0
-                global rhs[idx0] += beta * u(0, y, tn1)
+                rhs[idx0] += beta * u(0, y, tn1)
             end
 
             if i == grid_size_in
                 @assert x + h == 1.0
-                global rhs[idx0] += beta * u(1.0,y,tn1)
+                rhs[idx0] += beta * u(1.0,y,tn1)
             end
 
             if j == 1 
                 @assert y - h == 0.0
-                global rhs[idx0] += beta * u(x, 0, tn1)
+                rhs[idx0] += beta * u(x, 0, tn1)
             end
 
             if j == grid_size_in 
                 @assert y + h == 1.0
-                global rhs[idx0] += beta * u(x, 1.0,tn1)
+                rhs[idx0] += beta * u(x, 1.0,tn1)
             end
         end
     end
 
     # Use conjugate gradient to solve for u_n+1
     # Set u_n as the inital guess. u_n will be updated in place
-    cg!(u_n,L,rhs,reltol = 1e-8)
+    cg!(u_n,L,rhs,Pl = PreConL, reltol = 1e-8)
 
     # Mesure the relative L2 error with reference solution
     for i in 1 : grid_size_in
         for j in 1 : grid_size_in
-            global u_ref[get_idx(i,j,grid_size_in)] = u(i * h, j * h, tn1)
+            u_ref[get_idx(i,j,grid_size_in)] = u(i * h, j * h, tn1)
         end
     end
 
@@ -176,7 +197,7 @@ for n in 0 : Int(1.0/dt)
         p2 = heatmap(reshape(u_n,grid_size_in,grid_size_in), aspect_ratio=:equal,c=:viridis, clims=(0, 2), title="Solution")
         plt = plot(p1, p2, layout=(1,2))
         savefig(plt,"res/$n.png")
-        p3 = heatmap(reshape(abs.(u_n - u_ref) ./ abs.(u_ref),grid_size_in,grid_size_in), aspect_ratio=:equal,c=:viridis, clims=(0, 1), title="Error")
+        p3 = heatmap(reshape(abs.(u_n - u_ref) ./ abs.(u_ref),grid_size_in,grid_size_in), aspect_ratio=:equal,c=:viridis, clims=(0, 0.01), title="Error")
         plt = plot(p3)
         savefig(plt,"res/erro_$n.png")
 
@@ -184,7 +205,7 @@ for n in 0 : Int(1.0/dt)
         p2 = surface(reshape(u_n,grid_size_in,grid_size_in), aspect_ratio=:equal, zlims=(0, 2), title="Solution")
         plt = plot(p1, p2, layout=(1,2))
         savefig(plt,"res/height_$n.png")
-        p3 = surface(reshape(abs.(u_n - u_ref) ./ abs.(u_ref),grid_size_in,grid_size_in), aspect_ratio=:equal, zlims=(0, 1), title="Error")
+        p3 = surface(reshape(abs.(u_n - u_ref) ./ abs.(u_ref),grid_size_in,grid_size_in), aspect_ratio=:equal, zlims=(0, 0.01), title="Error")
         plt = plot(p3)
         savefig(plt,"res/height_erro_$n.png")
     end
